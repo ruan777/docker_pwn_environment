0a3c7965575954b33273d29ac39c4b1eff0b6a16
ruan777 <395000493@qq.com>
2020-08-13 13:16:47 +0800

    ubuntu20 env add zsh

--- ubuntu20/run.sh
+++ ubuntu20/run.sh
@@ -3,4 +3,4 @@
 # build image
 docker build -t pwn_env2004 .
 # run image
-docker run --rm -it -v `pwd`:/root/challenge  --name pwn_env_2004  pwn_env2004 /bin/bash
+docker run --rm -it -v `pwd`:/root/challenge  --name pwn_env_2004  pwn_env2004 
